fb90328c2dd7df1bde4d33afe39d9e8adb6a6692
Mainak Paul <mainakpaul2005@gmail.com>
2026-07-15 07:55:28 +0530

    Update 8queen.c

--- DAA Assignments/8queen.c
+++ DAA Assignments/8queen.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
-int isSafe(int board[8], int row, int col) {
+int isSafe(int board[], int row, int col) {
     for (int i = 0; i < row; i++) {
-        if (board[i] == col || board[i] - i == col - row || board[i] + i == col + row)
+        if (board[i] == col ||
+            board[i] - i == col - row ||
+            board[i] + i == col + row)
             return 0;
     }
     return 1;
 }
 
-int solve(int board[8], int row) {
-    if (row == 8)
+int solve(int board[], int row, int n) {
+    if (row == n)
         return 1;
 
-    for (int col = 0; col < 8; col++) {
+    for (int col = 0; col < n; col++) {
         if (isSafe(board, row, col)) {
             board[row] = col;
-            if (solve(board, row + 1))
+
+            if (solve(board, row + 1, n))
                 return 1;
         }
     }
     return 0;
 }
 
-void printBoard(int board[8]) {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
+void printBoard(int board[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             if (board[i] == j)
                 printf("Q ");
             else
@@ -35,24 +38,19 @@ void printBoard(int board[8]) {
 }
 
 int main() {
-    int board[8] = {0};
+    int n;
 
-    if (solve(board, 0)) {
-        printf("8-Queen solution:\n");
-        printBoard(board);
+    printf("Enter number of queens: ");
+    scanf("%d", &n);
+
+    int board[n];
+
+    if (solve(board, 0, n)) {
+        printf("%d-Queen solution:\n", n);
+        printBoard(board, n);
     } else {
         printf("No solution found.\n");
     }
-
-    printf("Backtracking explores solutions and prunes invalid states early.\n");
-
-    /*
-    Sample Output:
-    8-Queen solution:
-    Q . . . . . . .
-    ...
-    Backtracking explores solutions and prunes invalid states early.
-    */
 
     return 0;
 }
